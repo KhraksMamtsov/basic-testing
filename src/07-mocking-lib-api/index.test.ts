dcc18968d3c7e7b29f867efc6aa4f79aa226eee7
maksim.khramtsov <maksim.khramtsov@btsdigital.kz>
2023-06-28 14:56:42 +0200

    feat: finish last task

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,52 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import { throttledGetDataFromApi } from './index';
+import axios from 'axios';
+
+jest.mock('axios');
+
+const mockedAxios = jest.mocked(axios);
 
 describe('throttledGetDataFromApi', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+    jest.resetAllMocks();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const getMock = jest.fn(async () => ({}));
+    mockedAxios.create.mockReturnValue({ get: getMock } as any);
+
+    await throttledGetDataFromApi('testRelativePath');
+    await jest.runAllTimersAsync();
+
+    expect(mockedAxios.create).toBeCalledTimes(1);
+    expect(mockedAxios.create).toBeCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const testRelativePath = 'testRelativePath';
+    const getMock = jest.fn(async () => ({}));
+    mockedAxios.create.mockReturnValue({ get: getMock } as any);
+
+    await throttledGetDataFromApi(testRelativePath);
+    await jest.runAllTimersAsync();
+
+    expect(getMock).toBeCalledTimes(1);
+    expect(getMock).toBeCalledWith(testRelativePath);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const testData = 'testData';
+    const getMock = jest.fn(async () => ({ data: testData }));
+    mockedAxios.create.mockReturnValue({ get: getMock } as any);
+
+    const responseData = await throttledGetDataFromApi('testRelativePath');
+    await jest.runAllTimersAsync();
+    expect(responseData).toBe(testData);
   });
 });
